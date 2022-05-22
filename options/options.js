2a83f6af3328dc0ebfd017985c02fcdaba9e78a0
Crowdbotics <team@crowdbotics.com>
2022-05-22 17:48:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "the_only_tiger_35379",
+  url: "https://the_only_tiger_35379.botics.co",
+  api: "https://the_only_tiger_35379.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
